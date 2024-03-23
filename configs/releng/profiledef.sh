39ecd2d7387f92a4982d5069b33e23bfde83b220
Tiago de Paula <tiagodepalves@gmail.com>
2024-03-23 19:39:41 -0300

    feat: remove other useless sounds

--- configs/releng/profiledef.sh
+++ configs/releng/profiledef.sh
@@ -22,5 +22,4 @@ file_permissions=(
   ["/root/.gnupg"]="0:0:700"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
 )
